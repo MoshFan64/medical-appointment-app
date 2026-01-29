34637a668f6302ed8372e114a525097ac5fefb37
MoshFan64 <canhel.balam@gmail.com>
2026-01-29 02:22:05 -0600

    Sidebar UI polished; Exercise 2 completed.

--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -1,12 +1,6 @@
 @php
     $links = [
         [
-            'name' => 'Dashboard',
-            'icon' => 'fa-solid fa-gauge-high',
-            'href' => route('admin.dashboard'),
-            'active' => request()->routeIs('admin.dashboard'),
-        ],
-        [
             'header' => 'Administrar clínica',
         ],
         [
@@ -39,13 +33,14 @@
     ];
 @endphp
 
+{{-- Usamos style="margin-top: 64px;" para forzar el descenso sin importar las clases --}}
 <aside id="logo-sidebar"
-    class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+    class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+    style="margin-top: 64px;"
     aria-label="Sidebar">
-    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
-        {{-- ELIMINÉ EL BLOQUE DEL LOGO DE AQUÍ PARA QUE NO APAREZCA DEBAJO DEL DASHBOARD --}}
 
-        <ul class="space-y-2 font-medium">
+    <div class="h-full px-3 pb-24 overflow-y-auto bg-white dark:bg-gray-800">
+        <ul class="space-y-2 font-medium pt-4">
             @foreach ($links as $index => $link)
                 <li>
                     @isset($link['header'])
@@ -60,14 +55,13 @@
                                 <i class="{{ $link['icon'] }}"></i>
                             </span>
                             <span class="flex-1 ms-3 text-left whitespace-nowrap">{{ $link['name'] }}</span>
-                            {{-- Icono de flecha para el submenú --}}
                             <i class="fa-solid fa-chevron-down text-xs ms-auto"></i>
                         </button>
-                        <ul id="dropdown-{{ $index }}" class="hidden py-2 space-y-2">
+                        <ul id="dropdown-{{ $index }}" class="hidden py-2 space-y-2 ml-4">
                             @foreach ($link['submenu'] as $sub)
                                 <li>
                                     <a href="{{ $sub['href'] }}"
-                                        class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                                        class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 text-sm">
                                         {{ $sub['name'] }}
                                     </a>
                                 </li>
@@ -75,7 +69,7 @@
                         </ul>
                     @else
                         <a href="{{ $link['href'] }}"
-                            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ $link['active'] ? 'bg-gray-100 dark:bg-gray-700 font-bold' : '' }}">
+                            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                             <span class="w-6 h-6 inline-flex justify-center items-center text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white">
                                 <i class="{{ $link['icon'] }}"></i>
                             </span>
